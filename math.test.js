56d2c658e5c4b8ab5068805f4adb71b245a8ae7b
Pham Tuan <pvtuan2707@gmail.com>
2023-03-29 11:10:17 +0700

    jest fail

--- math.test.js
+++ math.test.js
@@ -1,14 +1,18 @@
 const {add,subtract,multiply,divide} = require("./math")
 describe("math.js test", ()=>{
+    
     if("should test add", ()=>{
         expect(add(2,2)).toBe(4)
     });
+    
     if("should test subtract", ()=>{
         expect(subtract(2,2)).toBe(0)
     });
+    
     if("should test multiply", ()=>{
         expect(multiply(2,2)).toBe(1)
     });
+    
     if("should test divide", ()=>{
         expect(divide(2,2)).toBe(4)
     });
